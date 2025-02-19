ead98ec6aea342fb40a41a75dac6de213f3214f8
TerenceKutzner <terence.kutzner@hotmail.com>
2025-02-19 15:05:02 -0300

    added divide function

--- scm.c
+++ scm.c
@@ -8,9 +8,23 @@ void multiplica(int a, int b) {
     printf("%d x %d = %d\n", a, b, a * b);
 }
 
+void divide(int a, int b) 
+{
+    if(b==0)
+    {
+        printf("erro divisão por 0\n");
+    }
+    else
+    {
+        printf("%d / %d = %f\n", a, b, (float)(a)/(float)(b));
+    }
+}
+
 int main() {
     printf("Olá, Estudante!\n");
     soma(5, 3);
     multiplica(5, 3);
+    divide(5,0);
+    divide(5,2);
     return 0;
 }
